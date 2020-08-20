70ad857d665bac77b1751eec6f142ca3fbee8469
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2020-08-20 03:55:40 +0200

    Display Balance Income  ExpenseDisplay Balance Income  Expense

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balacne = document.querySelector('#balance');
-const moneyPlus = document.querySelector('#monay-plus');
-const monyMinus = document.querySelector('#moeny-minus');
+const moneyPlus = document.querySelector('#money-plus');
+const monyMinus = document.querySelector('#money-minus');
 const list = document.querySelector('#list');
 const form = document.querySelector('#form');
 const text = document.querySelector('#text');
@@ -36,10 +36,30 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// Update the balacne, income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balacne.innerText = `$${total}`;
+  moneyPlus.innerText = `$${income}`;
+  monyMinus.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
